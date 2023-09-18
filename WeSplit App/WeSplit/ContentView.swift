8a1e3c353d9ed6c3e98785f5c08149c863db241b
MianAnswer <miananswer@hotmail.com>
2023-09-18 11:16:55 -0400

    WeSplit app

---
+++ WeSplit App/WeSplit/ContentView.swift
@@ -0,0 +1,91 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Mian Answer on 9/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    private var totalAmount: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        return checkAmount + tipValue
+    }
+    private var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        
+        return totalAmount / peopleCount
+    }
+    private let currencyFormat: FloatingPointFormatStyle<Double>.Currency = .currency(code: Locale.current.currency?.identifier ?? "USD")
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: currencyFormat)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                } header: {
+                    Text("Amount")
+                        .foregroundColor(.accentColor)
+                }
+
+                Section {
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0..<101) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                } header: {
+                    Text("Tip Percentage")
+                        .foregroundColor(.accentColor)
+                }
+
+                Section {
+                    Text(totalAmount, format: currencyFormat)
+                } header: {
+                    Text("Total Amount")
+                        .foregroundColor(.accentColor)
+                }
+        
+                Section {
+                    Text(totalPerPerson, format: currencyFormat)
+                } header: {
+                    Text("Amount Per Person")
+                        .foregroundColor(.accentColor)
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
